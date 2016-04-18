543f84f122b0d033cb633e9e1b35485d635c1192
Angad Nadkarni <angad@gethullo.com>
2016-04-18 13:19:54 +0530

    Initial commit

---
+++ blob64.js
@@ -0,0 +1,30 @@
+"use strict";
+var Blob64 = (function () {
+    function Blob64() {
+    }
+    Blob64.serialize = function (blob, onFinish) {
+        var reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = function () {
+            onFinish(reader.result);
+        };
+    };
+    Blob64.deserialize = function (str) {
+        var indexOfComma = str.indexOf(",");
+        var contentType = str.substr(0, indexOfComma + 1) || "";
+        var byteChars = atob(str.substr(indexOfComma));
+        var sliceSize = 512;
+        var byteArrays = [];
+        for (var offset = 0; offset < byteChars.length; offset += sliceSize) {
+            var slice = byteChars.slice(offset, offset + sliceSize);
+            var byteNumbers = new Array(slice.length);
+            for (var i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+            byteArrays.push(new Uint8Array(byteNumbers));
+        }
+        return new Blob(byteArrays, { type: contentType });
+    };
+    return Blob64;
+}());
+exports.Blob64 = Blob64;
---
+++ blob64.ts
@@ -0,0 +1,32 @@
+export class Blob64 {
+  static serialize(blob: Blob, onFinish: (str: string) => void) {
+    // Ref: http://stackoverflow.com/a/18650249/382564
+    let reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = () => {
+      onFinish(reader.result);
+    }
+  }
+
+  static deserialize(str: string): Blob {
+    // Ref: http://stackoverflow.com/a/16245768/382564
+    let indexOfComma = str.indexOf(",");
+    let contentType = str.substr(0, indexOfComma + 1) || "";
+    let byteChars = atob(str.substr(indexOfComma));
+    let sliceSize = 512;
+
+    let byteArrays = [];
+    for (var offset = 0; offset < byteChars.length; offset += sliceSize) {
+      let slice = byteChars.slice(offset, offset + sliceSize);
+      let byteNumbers = new Array(slice.length);
+
+      for (var i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      byteArrays.push(new Uint8Array(byteNumbers));
+    }
+
+    return new Blob(byteArrays, {type: contentType});
+  }
+}
